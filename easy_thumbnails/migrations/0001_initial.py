2660e0f9cdc7e100ed7fae0d0b9d3cd0e6e7a2ca
RJ Regenold <rjregenold@gmail.com>
2010-09-30 19:15:08 -0500

    Removed old migrations.

--- easy_thumbnails/migrations/0001_initial.py
+++ easy_thumbnails/migrations/0001_initial.py
@@ -1,71 +1,58 @@
-
+# encoding: utf-8
+import datetime
 from south.db import db
+from south.v2 import SchemaMigration
 from django.db import models
-from easy_thumbnails.models import *
 
-class Migration:
-    
+class Migration(SchemaMigration):
+
     def forwards(self, orm):
         
         # Adding model 'Source'
         db.create_table('easy_thumbnails_source', (
-            ('id', orm['easy_thumbnails.Source:id']),
-            ('storage', orm['easy_thumbnails.Source:storage']),
-            ('name', orm['easy_thumbnails.Source:name']),
-            ('modified', orm['easy_thumbnails.Source:modified']),
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('storage_hash', self.gf('django.db.models.fields.CharField')(max_length=40, db_index=True)),
+            ('name', self.gf('django.db.models.fields.CharField')(max_length=255, db_index=True)),
+            ('modified', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime(2010, 10, 1, 0, 9, 47, 385857))),
         ))
         db.send_create_signal('easy_thumbnails', ['Source'])
-        
-        # Adding model 'Storage'
-        db.create_table('easy_thumbnails_storage', (
-            ('hash', orm['easy_thumbnails.Storage:hash']),
-            ('pickle', orm['easy_thumbnails.Storage:pickle']),
-        ))
-        db.send_create_signal('easy_thumbnails', ['Storage'])
-        
+
         # Adding model 'Thumbnail'
         db.create_table('easy_thumbnails_thumbnail', (
-            ('id', orm['easy_thumbnails.Thumbnail:id']),
-            ('storage', orm['easy_thumbnails.Thumbnail:storage']),
-            ('name', orm['easy_thumbnails.Thumbnail:name']),
-            ('modified', orm['easy_thumbnails.Thumbnail:modified']),
-            ('source', orm['easy_thumbnails.Thumbnail:source']),
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('storage_hash', self.gf('django.db.models.fields.CharField')(max_length=40, db_index=True)),
+            ('name', self.gf('django.db.models.fields.CharField')(max_length=255, db_index=True)),
+            ('modified', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime(2010, 10, 1, 0, 9, 47, 385857))),
+            ('source', self.gf('django.db.models.fields.related.ForeignKey')(related_name='thumbnails', to=orm['easy_thumbnails.Source'])),
         ))
         db.send_create_signal('easy_thumbnails', ['Thumbnail'])
-        
-    
-    
+
+
     def backwards(self, orm):
         
         # Deleting model 'Source'
         db.delete_table('easy_thumbnails_source')
-        
-        # Deleting model 'Storage'
-        db.delete_table('easy_thumbnails_storage')
-        
+
         # Deleting model 'Thumbnail'
         db.delete_table('easy_thumbnails_thumbnail')
-        
-    
-    
+
+
     models = {
         'easy_thumbnails.source': {
+            'Meta': {'object_name': 'Source'},
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'modified': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2009, 11, 22, 21, 16, 23, 523494)'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
-            'storage': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['easy_thumbnails.Storage']"})
-        },
-        'easy_thumbnails.storage': {
-            'hash': ('django.db.models.fields.CharField', [], {'max_length': '40', 'primary_key': 'True'}),
-            'pickle': ('django.db.models.fields.TextField', [], {})
+            'modified': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2010, 10, 1, 0, 9, 47, 385857)'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '255', 'db_index': 'True'}),
+            'storage_hash': ('django.db.models.fields.CharField', [], {'max_length': '40', 'db_index': 'True'})
         },
         'easy_thumbnails.thumbnail': {
+            'Meta': {'object_name': 'Thumbnail'},
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'modified': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2009, 11, 22, 21, 16, 23, 523494)'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2010, 10, 1, 0, 9, 47, 385857)'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '255', 'db_index': 'True'}),
             'source': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'thumbnails'", 'to': "orm['easy_thumbnails.Source']"}),
-            'storage': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['easy_thumbnails.Storage']"})
+            'storage_hash': ('django.db.models.fields.CharField', [], {'max_length': '40', 'db_index': 'True'})
         }
     }
-    
+
     complete_apps = ['easy_thumbnails']
